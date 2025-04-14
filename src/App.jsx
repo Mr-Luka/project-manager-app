e3276cb337c5c37a8ea0be944c2ae78152068683
Luka <luka.luka.luka93@gmail.com>
2025-04-14 11:37:05 -0700

    added click events to the create new project and addProject to open a component AddProject where user can input the project info

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,38 @@ import './index.css';
 import {useState} from 'react';
 import { v4 as uuidv4 } from 'uuid'; //Importing a UUID generator
 
+import NoProjectSelected from './assets/components/NoProjectSelected';
+import AddProject from './assets/components/AddProject';
+
 
 function App() {
+  const [createProjectButton, setCreateProjectButton] = useState(false);
+
+  function handleCreateProject(){
+    setCreateProjectButton(true);
+  }
+
 
   return (
     <>
        <div className="projects-wrapper">
         <div className="add-projects">
           <h2>YOUR PROJECTS</h2>
-          <button>+ Add Project</button>
+          <button onClick={handleCreateProject}>+ Add Project</button>
           <div className='saved-projects'>
             <ol className='ol-saved-projects'>
     
             </ol>
           </div>
         </div>
+        {createProjectButton ? 
+         <AddProject />
+         : 
+         <NoProjectSelected 
+            createNewProject={handleCreateProject}
+          />
+        }
+
 
 
       </div>
--- src/assets/components/NoProjectSelected.jsx
+++ src/assets/components/NoProjectSelected.jsx
@@ -1,13 +1,13 @@
 import picture from '../no-projects.png';
 
-export default function NoProjectSelected(){
+export default function NoProjectSelected({createNewProject}){
 
     return (
         <div className='no-project-selected'>
             <img src={picture} alt="No projects selected" />
             <h3>No Project Selected</h3>
             <p>Select a project or get started with a new one</p>
-            <button>Create new project</button>
+            <button onClick={createNewProject}>Create new project</button>
         </div>
     )
 }
