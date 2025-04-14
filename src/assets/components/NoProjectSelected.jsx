7f35072c34c2ae37b5e02419acc4e679018c11cd
Luka <luka.luka.luka93@gmail.com>
2025-04-14 15:09:36 -0700

    finished project

--- src/assets/components/NoProjectSelected.jsx
+++ src/assets/components/NoProjectSelected.jsx
@@ -1,13 +1,13 @@
 import picture from '../no-projects.png';
 
-export default function NoProjectSelected({createNewProject}){
+export default function NoProjectSelected({onClick}){
 
     return (
         <div className='no-project-selected'>
             <img src={picture} alt="No projects selected" />
             <h3>No Project Selected</h3>
             <p>Select a project or get started with a new one</p>
-            <button onClick={createNewProject}>Create new project</button>
+            <button onClick={onClick}>Create new project</button>
         </div>
     )
 }
--- src/assets/components/Project.jsx
+++ src/assets/components/Project.jsx
@@ -1,25 +1,48 @@
+import {useState} from 'react';
 import { convertDate } from '../utils/convertDate.js';
 
-export default function Project({title, description, date}){
+export default function Project({title, description, date, deleteProject}){
+  const [ tasks, setTasks ] = useState([]);
+  const [ newTask, setNewTask ] = useState('');
+
   const convertedDate = convertDate(date);
+
+  function handleInput(e){
+    setNewTask(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    if(newTask.trim() !== ''){
+      setTasks(prevTask=> [...prevTask, newTask]);
+      setNewTask('');
+    }
+  }
+
+  function clearTask(taskIndex) {
+    const filteredTasks = tasks.filter((_, index)=> index !== taskIndex)
+    setTasks(filteredTasks)
+  }
  
     return (
         <div className='project'>
           <div className='title-and-button'>
             <h1>{title}</h1>
-            <button>Delete</button>
+            <button onClick={deleteProject}>Delete</button>
           </div>
           <p>{convertedDate}</p>
           <p>{description}</p>
           <hr></hr>
           <h2>Tasks</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="tasks">
-            <input></input>
+            <input onChange={handleInput}></input>
             <button type='submit'>Add Task</button>
           </div>
-          <ol className='tasks-ol'>
-            <li>This project does no have any tasks yet.</li>
+          <ol className={`tasks-ol ${tasks.length > 0 ? 'active-tasks' : ''}`}>
+            {tasks.length > 0 ? (tasks.map((task, taskIndex) => (
+      <li key={taskIndex}><p>{task}</p><button onClick={()=> clearTask(taskIndex)}>Clear</button></li>
+    ))) : <li>This project does no have any tasks yet.</li>}
           </ol>
           </form>
         </div>
