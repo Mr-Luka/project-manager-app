8558ccca5bab532229560b708a57d68ab72687ce
Luka <luka.luka.luka93@gmail.com>
2025-04-15 14:39:17 -0700

    finished project, added click button to add task, input, and map through the array of tasks, for each it will give me a listed item, and each one has a delete button function

--- src/assets/components/Project.jsx
+++ src/assets/components/Project.jsx
@@ -2,6 +2,29 @@ import {useState} from 'react';
 import { convertDate } from '../utils/convertDate.js';
 
 export default function Project({title, description, date, remove}){
+  const [inputTask, setInputTask] = useState('');
+  const [taskData, setTaskData] = useState([]);
+
+  const formattedDate = convertDate(date);
+
+  function handleInput(e){
+    setInputTask(e.target.value);
+  }
+
+  function handleSubmitForm(e){
+    e.preventDefault();
+    if(inputTask.trim() !== ''){
+      setTaskData(prevData => {
+        return [...prevData, inputTask];
+      })
+    }
+    setInputTask('');
+  }
+
+  function deleteTask(keyIndex){
+    const updatedTaskArray = taskData.filter((task, taskIndex) => task[taskIndex] !== task[keyIndex]);
+    setTaskData(updatedTaskArray);
+  }
 
     return (
         <div className='project'>
@@ -9,17 +32,23 @@ export default function Project({title, description, date, remove}){
             <h1>{title}</h1>
             <button onClick={remove}>Delete</button>
           </div>
-          <p>{date}</p>
+          <p>{formattedDate}</p>
           <p>{description}</p>
           <hr></hr>
           <h2>Tasks</h2>
-          <form>
+          <form onSubmit={handleSubmitForm}>
             <div className="tasks">
-            <input></input>
+            <input onChange={handleInput} value={inputTask}></input>
             <button type='submit'>Add Task</button>
           </div>
-          <ol className='tasks-ol'>
-            <li>This project does no have any tasks yet.</li>
+          <ol className={`tasks-ol ${taskData.length > 0 ? 'active-tasks' : ''}` }>
+            {taskData.length > 0
+            ?
+              (taskData.map((task, taskIndex)=> 
+              <li key={taskIndex}><p>{task}</p><button onClick={()=> deleteTask(taskIndex)}>Delete</button></li> ))
+            : 
+              (<li>This project does no have any tasks yet.</li>)
+            }
           </ol>
           </form>
         </div>
